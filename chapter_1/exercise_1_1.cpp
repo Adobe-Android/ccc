2a60d2e991d0f64280f4e466c7fb295656043fb3
Adobe-Android <tngamerboy@gmail.com>
2020-06-12 17:21:55 -0500

    Finished exercise 1

--- chapter_1/exercise_1_1.cpp
+++ chapter_1/exercise_1_1.cpp
@@ -1,11 +1,15 @@
 #include <cstdio>
+#include <cstdlib>
 
 int absolute_value(int x) {
-  // Your code here
-  return 0;
+  if(x > 0) {
+    return x;
+  } else {
+    return x * -1;
+  }
 }
 
-int main() {
-  int my_num = -10;
+int main(int argc, const char* argv[]) {
+  int my_num = atoi(argv[1]);
   printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
 }
