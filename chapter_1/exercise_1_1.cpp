0f307f91d82640dae7363e277689a39dc4d9ef94
Adobe-Android <tngamerboy@gmail.com>
2020-06-13 17:41:35 -0500

    Finished chapter 1 and 2 exercises

--- chapter_1/exercise_1_1.cpp
+++ chapter_1/exercise_1_1.cpp
@@ -9,7 +9,17 @@ int absolute_value(int x) {
   }
 }
 
+// 1-4.
+int sum (int a, int b) {
+  return a + b;
+}
+
 int main(int argc, const char* argv[]) {
   int my_num = atoi(argv[1]);
   printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+  
+  // 1-4.
+  int a{1};
+  int b{2};
+  printf("The sum of %d and %d is %d.\n", a, b, sum(a, b));
 }
